44d16a0d0c3f10d258e9c08bd801d124fafa0688
Squishy <iacampoaw@gmail.com>
2019-03-01 12:54:40 -0500

    pm2

---
+++ ecosystem.config.js
@@ -0,0 +1,31 @@
+module.exports = {
+  apps : [{
+    name: 'Timing Server',
+    script: 'index.js',
+
+    // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
+    args: 'one two',
+    instances: 1,
+    autorestart: true,
+    watch: false,
+    max_memory_restart: '1G',
+    env: {
+      NODE_ENV: 'development'
+    },
+    env_production: {
+      NODE_ENV: 'production'
+    }
+  }],
+
+  deploy : {
+    production : {
+      user : 'ubuntu',
+      host : 'ec2-34-216-180-39.us-west-2.compute.amazonaws.com',
+      key: '~/.ssh/id_rsa .pem',
+      ref  : 'origin/master',
+      repo : 'https://github.com/SquishyRock/timingServer.git',
+      path : '/home/ubuntu/timingServer',
+      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production'
+    }
+  }
+};
